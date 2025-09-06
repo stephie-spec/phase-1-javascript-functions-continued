cffc5cd6fc6611609e571502750c37f3735c2cc9
stephie-spec <compwork254@gmail.com>
2025-09-06 17:41:17 +0300

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(highlight = "*") {
+  return function(adjective = "hard") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
